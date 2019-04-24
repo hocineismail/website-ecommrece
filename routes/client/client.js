e8dec9961aa5621c184df6ebd80397d6a8528f0f
hocineimail <hocineismail75@gmail.com>
2019-04-25 00:43:28 +0200

    Create routes list of shopping

--- routes/client/client.js
+++ routes/client/client.js
@@ -4,4 +4,9 @@ var client = express.Router();
 client.get("/Compte",(req,res)=> {
     res.render('Client/compteClient')
 })
+
+
+client.get("/List/achats",(req,res)=> {
+    res.render('Client/itemProduit')
+})
 module.exports = client;
