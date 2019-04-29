fa71c1cb6c92a259596ce1bc15d61cae0199859c
hocineimail <hocineismail75@gmail.com>
2019-04-30 00:49:54 +0200

    Updating

--- routes/client/client.js
+++ routes/client/client.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const client = express.Router();
-const Lists = require("../../models/lists")
+const Lists = require("../../models/lists");
+const async = require('async')
 
 client.get("/Compte",(req,res)=> {
     res.render('Client/compteClient')
@@ -10,16 +11,41 @@ client.get("/Compte",(req,res)=> {
 client.get("/List/achats",(req,res)=> {
     res.render('Client/itemProduit')
 })
-
-client.post("/AddToList", (req, res) => {
-    
-    const AddTList = new Lists({
-        Quantite: req.body.Quantite,      
-        produit: req.body._id,
-        user: req.user._id   
-    });AddTList.save((err, siccess) => {
-        console.log(siccess)
+client.get("/D", (req,res) => {
+    Lists.remove({}, (err, deletee) => {
+        if (deletee) {
+           console.log('suprimi')
+        }
     })
+    res.redirect("/")
+    
+})
+client.post("/AddToList", async  (req, res) => {
+    const produit = await Lists.findOne({Paye: false, produit: req.body._id, user: req.user._id})
+    Lists.find({}, (err, ress ) => {
+        console.log(ress)
+    })
+    if (produit) {
+        Lists.findOne({Paye: false, produit: req.body._id, user: req.user._id}, (err, Exist) => {
+            console.log(Exist)
+            const Quantite =  parseInt(Exist.Quantite);
+            Exist.Quantite =  Quantite + parseInt(req.body.Quantite);
+            Exist.save();
+            console.log(Exist)
+            return
+        })     
+    } else  {
+        const AddTList = new Lists({
+            Quantite: req.body.Quantite,      
+            produit: req.body._id,
+            user: req.user._id   
+        });AddTList.save((err, siccess) => {
+            if (!siccess) {
+                console.log("not saving")
+            }
+        })
+    }
+
 
 })
 module.exports = client;
