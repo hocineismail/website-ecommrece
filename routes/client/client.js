7766302396448d25b704a31e7457c7ee9418155b
hocineimail <hocineismail75@gmail.com>
2019-05-03 23:47:56 +0200

    added update of  list

--- routes/client/client.js
+++ routes/client/client.js
@@ -5,26 +5,37 @@ const client = express.Router();
 const Lists = require("../../models/lists");
 const async = require('async')
 const Produit = require("../../models/produit")
+const flash = require("connect-flash"); 
+
+client.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.errors = req.flash("error");
+    res.locals.infos = req.flash("info");
+    next();
+   })
+   
 client.get("/Compte",(req,res)=> {
     res.render('Client/compteClient')
 })
 
-client.post("/update/achat/_id" ,(req, res) => {
-    Lists.findOne({user: req.user._id, produit: req.params._id}, (err, Update) => {
+client.post("/update/achat/:_id" ,(req, res) => {
+  
+    Lists.findOne({user: req.user._id, _id: req.params._id}, (err, Update) => {
+        console.log(Update)
+ 
         if (Update) {
             Update.Quantite = req.body.Quantite;
             Update.save();
-            req.redirect("/List/achats")
-        }
+            req.flash("info", "Updating ");
+            res.redirect("/List/achats")
+        } else  { 
+        req.flash("error", "ce User existe");
+        res.redirect("/List/achats")}
     })
 })
 client.get("/List/achats",(req,res)=> {
     Lists.find({user: req.user._id}).populate({path: 'produit', populate: {path: 'image'} }).exec((err, lists) => {
-        
-  
-       if (lists) {
-         
-           console.log(lists)
+        if (lists) {
            res.render('Client/itemProduit', {List: lists})
        }
     })
